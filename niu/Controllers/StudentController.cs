7b678e973d7ad18e050d2fdba7c5ca0deca97fc4
Pravin Kandala <pravinkandala@hotmail.com>
2015-12-30 14:43:47 -0600

    Added search functionality

--- niu/Controllers/StudentController.cs
+++ niu/Controllers/StudentController.cs
@@ -21,12 +21,16 @@ namespace niu.Controllers
         //    return View(db.Students.ToList());
         //}
 
-        public ActionResult Index(string sortOrder)
+        public ActionResult Index(string sortOrder, string searchString)
         {
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
             var students = from s in db.Students
                            select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));
+            }
             switch (sortOrder)
             {
                 case "name_desc":
