be8c3876c0a406f00d9ab665e859dec17b6f21bd
Pravin Kandala <pravinkandala@hotmail.com>
2015-12-30 16:55:21 -0600

    About page shows Student Statistics now

--- niu/Controllers/HomeController.cs
+++ niu/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using niu.DAL;
+using niu.ViewModels;
 
 namespace niu.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -17,9 +20,17 @@ namespace niu.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            //ViewBag.Message = "Your app description page.";
 
-            return View();
+            //return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -27,6 +38,11 @@ namespace niu.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
